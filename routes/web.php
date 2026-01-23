658061ed9abc3543754ab0b6c19a176a5c1ac885
Shaharyar <ss.aurangzeb007@gmail.com>
2026-01-23 11:12:56 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/bobas');
 });
 
 Route::get('/bobas', [BobaController::class, 'index']);
@@ -14,7 +14,7 @@ Route::get('/bobas/create', [BobaController::class, 'create'])->middleware(['aut
 Route::get('/bobas/about', [BobaController::class, 'about']);
 
 Route::post('/bobas', [BobaController::class, 'store']);
-Route::get('/bobas/{id}', [BobaController::class, 'show']);
+// Route::get('/bobas/{id}', [BobaController::class, 'show']);
 Route::get('/bobas/{id}/edit', [BobaController::class, 'edit'])->middleware(['auth', 'can:admin-access']);
 Route::get('/bobas/{id}/addReview', [ReviewController::class, 'create'])->middleware(['auth', 'can:admin-or-user']);
 
