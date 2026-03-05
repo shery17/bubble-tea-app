ba0b9e1a3b0b0087e8b5a906523947b69e9f5994
Shaharyar <ss.aurangzeb007@gmail.com>
2026-03-05 02:10:37 +0000

    Update AppServiceProvider.php
    To allow railway to use tailwind on deployment. Forced scheme to https instead of http.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // forced scheme to use https instead of http for railway deployment
+        URL::forceScheme('https');
+    
         // Gate for admin access (full access)
         Gate::define('admin-access', function (User $user) {
             return $user->role_id === 1; // role_id = 1 is for admin
