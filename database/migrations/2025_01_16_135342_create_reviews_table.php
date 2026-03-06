0a2aab461928eec2a3cf0ec792b54a55040769eb
Shaharyar <ss.aurangzeb007@gmail.com>
2026-03-06 05:33:52 +0000

    Update 2025_01_16_135342_create_reviews_table.php

--- database/migrations/2025_01_16_135342_create_reviews_table.php
+++ database/migrations/2025_01_16_135342_create_reviews_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('content');
             $table->integer('rating');
+            $table->timestamps();
         });
     }
 
